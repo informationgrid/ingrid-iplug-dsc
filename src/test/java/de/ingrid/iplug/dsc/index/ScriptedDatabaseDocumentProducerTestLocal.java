5707e740a860a3ceb93c05bdc6d10326764d357b
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-02-22 15:37:11 +0000

    iplug-dsc-scripted: - added local testcase for mapping record(s) from local IGC catalog in mySql

---
+++ src/test/java/de/ingrid/iplug/dsc/index/ScriptedDatabaseDocumentProducerTestLocal.java
@@ -0,0 +1,63 @@
+package de.ingrid.iplug.dsc.index;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.lucene.document.Document;
+import org.springframework.core.io.ClassPathResource;
+
+import de.ingrid.iplug.dsc.index.mapper.IRecordMapper;
+import de.ingrid.iplug.dsc.index.mapper.ScriptedDocumentMapper;
+import de.ingrid.iplug.dsc.index.producer.PlugDescriptionConfiguredDatabaseRecordSetProducer;
+import de.ingrid.utils.PlugDescription;
+import de.ingrid.utils.xml.PlugdescriptionSerializer;
+
+public class ScriptedDatabaseDocumentProducerTestLocal extends TestCase {
+
+    public void testScriptedDatabaseDocumentProducer() throws Exception {
+        File plugDescriptionFile = new File(
+                "src/test/resources/plugdescription_db_test_local.xml");
+        PlugDescription pd = new PlugdescriptionSerializer()
+                .deSerialize(plugDescriptionFile);
+
+        PlugDescriptionConfiguredDatabaseRecordSetProducer p = new PlugDescriptionConfiguredDatabaseRecordSetProducer();
+        String sql = "SELECT distinct obj.id " +
+        		"FROM " +
+        		"object_node oNode, " +
+        		"t01_object obj " +
+        		"WHERE " +
+        		"oNode.obj_id_published = obj.id " +
+        		"AND obj.publish_id = 1 "
+        		// single record
+        		+ "AND obj.id = 3776"
+        		;
+        p.setRecordSql(sql);
+        p.configure(pd);
+
+        ScriptedDocumentMapper m = new ScriptedDocumentMapper();
+        m.setMappingScript(new ClassPathResource("scripts/record2index_database_test_local.js"));
+        m.setCompile(false);
+
+        List<IRecordMapper> mList = new ArrayList<IRecordMapper>();
+        mList.add(m);
+        
+        DscDocumentProducer dp = new DscDocumentProducer();
+        dp.setRecordSetProducer(p);
+        dp.setRecordMapperList(mList);
+
+        if (dp.hasNext()) {
+            while (dp.hasNext()) {
+                Document doc = dp.next();
+                assertNotNull(doc);
+                // when single record fetched !
+                assertEquals(doc.getField("obj_uuid").stringValue(), "7AC6048A-7018-11D3-A599-C70A0FEBD4FC");
+            }
+        } else {
+            fail("No document produced");
+        }
+    }
+    
+}
