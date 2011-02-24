8d397a9324a7ff93edcac61e5cca4164c004d524
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-02-24 10:09:51 +0000

    iplug-dsc-scripted: - t01_object mapped in script ... WORK IN PROGRESS !

--- src/main/resources/mapping/igc-3.0.0_to_lucene.js
+++ src/main/resources/mapping/igc-3.0.0_to_lucene.js
@@ -26,8 +26,9 @@ if (!(sourceRecord instanceof DatabaseSourceRecord)) {
 
 var id = sourceRecord.get(DatabaseSourceRecord.ID);
 
-// ---------- t01_object ----------
  
+// ---------- t01_object ----------
+
 var rows = SQL.all("SELECT * FROM t01_object WHERE id=?", [id]);
 for (i=0; i<rows.size(); i++ ) {
     var row = rows.get(i);
@@ -39,6 +40,12 @@ for (i=0; i<rows.size(); i++ ) {
         IDX.add(colName, row.get(colName));
 	}
 */
+    addT01Object(row);
+
+    // TODO: - Coord mapping
+}
+
+function addT01Object(row) {
     IDX.add("t01_object.id", row.get("id"));
     IDX.add("t01_object.obj_id", row.get("obj_uuid"));
     IDX.add("title", row.get("obj_name"));
@@ -49,10 +56,38 @@ for (i=0; i<rows.size(); i++ ) {
     IDX.add("t01_object.info_note", row.get("info_note"));
     IDX.add("t01_object.loc_descr", row.get("loc_descr"));
 
-    IDX.add("t01_object.time_type", row.get("time_type"));
+    // time: add t0, t1, t2 fields dependent from time_type
     TRANSF.processTimeFields(row.get("time_from"), row.get("time_to"), row.get("time_type"));
-}
+    IDX.add("t01_object.time_type", row.get("time_type"));
+    IDX.add("t01_object.time_descr", row.get("time_descr"));
+    IDX.add("t01_object.time_period", row.get("time_period"));
+    IDX.add("t01_object.time_interval", row.get("time_interval"));
+    IDX.add("t01_object.time_status", row.get("time_status"));
+    IDX.add("t01_object.time_alle", row.get("time_alle"));
 
+    IDX.add("t01_object.publish_id", row.get("publish_id"));
+    IDX.add("t01_object.dataset_alternate_name", row.get("dataset_alternate_name"));
+    IDX.add("t01_object.dataset_character_set", row.get("dataset_character_set"));
+    IDX.add("t01_object.dataset_usage", row.get("dataset_usage"));
+    IDX.add("t01_object.data_language_key", row.get("data_language_key"));
+    IDX.add("t01_object.data_language", row.get("data_language"));
+    IDX.add("t01_object.metadata_character_set", row.get("metadata_character_set"));
+    IDX.add("t01_object.metadata_standard_name", row.get("metadata_standard_name"));
+    IDX.add("t01_object.metadata_standard_version", row.get("metadata_standard_version"));
+    IDX.add("t01_object.metadata_language_key", row.get("metadata_language_key"));
+    IDX.add("t01_object.metadata_language", row.get("metadata_language"));
+    IDX.add("t01_object.vertical_extent_minimum", row.get("vertical_extent_minimum"));
+    IDX.add("t01_object.vertical_extent_maximum", row.get("vertical_extent_maximum"));
+    IDX.add("t01_object.vertical_extent_unit", row.get("vertical_extent_unit"));
+    IDX.add("t01_object.vertical_extent_vdatum", row.get("vertical_extent_vdatum"));
+    IDX.add("t01_object.ordering_instructions", row.get("ordering_instructions"));
+    IDX.add("t01_object.is_catalog_data", row.get("is_catalog_data"));
+    IDX.add("t01_object.create_time", row.get("create_time"));
+    IDX.add("t01_object.mod_time", row.get("mod_time"));
+    IDX.add("t01_object.mod_uuid", row.get("mod_uuid"));
+    IDX.add("t01_object.responsible_uuid", row.get("responsible_uuid"));
+}
+/*
 function hasValue(val) {
     if (typeof val == "undefined") {
         return false; 
@@ -64,3 +99,4 @@ function hasValue(val) {
       return true;
     }
 }
+*/
