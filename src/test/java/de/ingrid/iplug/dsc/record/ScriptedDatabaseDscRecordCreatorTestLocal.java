fb4a710f5f9d151927f8a8edffbce8e6589b9fc2
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-03-10 19:18:34 +0000

    iplug-dsc-scripted: - extend ScriptedDatabaseDscRecordCreatorTestLocal to test all UDK classes !

--- src/test/java/de/ingrid/iplug/dsc/record/ScriptedDatabaseDscRecordCreatorTestLocal.java
+++ src/test/java/de/ingrid/iplug/dsc/record/ScriptedDatabaseDscRecordCreatorTestLocal.java
@@ -42,31 +42,43 @@ public class ScriptedDatabaseDscRecordCreatorTestLocal extends TestCase {
         dc.setRecordProducer(p);
         dc.setRecord2IdfMapperList(mList);
 
-        String t01ObjectId = "3778";
-
-        Document idxDoc = new Document();
-        idxDoc.add(new Field("t01_object.id", t01ObjectId, Field.Store.YES, Field.Index.ANALYZED));
-        Record r = dc.getRecord(idxDoc);
-        assertNotNull(r.get("data"));
-        assertTrue(r.getString("compressed").equals("false"));
-        System.out.println("Size of uncompressed IDF document: " + r.getString("data").length());
+        String[] t01ObjectIds = new String[] {
+        		"6667",		// class 0 = Organisationseinheit/Fachaufgabe
+        		"3778",		// class 1 = Geo-Information/Karte
+//        		"6672",		// class 1 = Geo-Information/Karte
+        		"3919",		// class 2 = Dokument/Bericht/Literatur
+        		"8781824",	// class 3 = Geodatendienst
+        		"3782",		// class 4 = Vorhaben/Projekt/Programm
+        		"3820",		// class 5 = Datensammlung/Datenbank
+        		"7897095",	// class 6 = Informationssystem/Dienst/Anwendung
+//        		"6685",		// class 6 = Informationssystem/Dienst/Anwendung
+        };
         
-        idxDoc = new Document();
-        idxDoc.add(new Field("t01_object.id", t01ObjectId, Field.Store.YES, Field.Index.ANALYZED));
-        dc.setCompressed(true);
-        r = dc.getRecord(idxDoc);
-        assertNotNull(r.get("data"));
-        assertTrue(r.getString("compressed").equals("true"));
-        System.out.println("Size of compressed IDF document: " + r.getString("data").length());
+        for (String t01ObjectId : t01ObjectIds) {
+            Document idxDoc = new Document();
+            idxDoc.add(new Field("t01_object.id", t01ObjectId, Field.Store.YES, Field.Index.ANALYZED));
+            dc.setCompressed(false);
+            Record r = dc.getRecord(idxDoc);
+            assertNotNull(r.get("data"));
+            assertTrue(r.getString("compressed").equals("false"));
+            System.out.println("Size of uncompressed IDF document: " + r.getString("data").length());
 
-        m2.setCompile(true);
-        idxDoc = new Document();
-        idxDoc.add(new Field("t01_object.id", t01ObjectId, Field.Store.YES, Field.Index.ANALYZED));
-        dc.setCompressed(true);
-        r = dc.getRecord(idxDoc);
-        assertNotNull(r.get("data"));
-        assertTrue(r.getString("compressed").equals("true"));
-        System.out.println("Size of compressed IDF document with compiled mapper script: " + r.getString("data").length());
+            idxDoc = new Document();
+            idxDoc.add(new Field("t01_object.id", t01ObjectId, Field.Store.YES, Field.Index.ANALYZED));
+            dc.setCompressed(true);
+            r = dc.getRecord(idxDoc);
+            assertNotNull(r.get("data"));
+            assertTrue(r.getString("compressed").equals("true"));
+            System.out.println("Size of compressed IDF document: " + r.getString("data").length());
+
+            m2.setCompile(true);
+            idxDoc = new Document();
+            idxDoc.add(new Field("t01_object.id", t01ObjectId, Field.Store.YES, Field.Index.ANALYZED));
+            dc.setCompressed(true);
+            r = dc.getRecord(idxDoc);
+            assertNotNull(r.get("data"));
+            assertTrue(r.getString("compressed").equals("true"));
+            System.out.println("Size of compressed IDF document with compiled mapper script: " + r.getString("data").length());
+        }
     }
-
 }
