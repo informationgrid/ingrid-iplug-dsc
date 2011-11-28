cfef9b9d8850c6671066132413b92b1fe14fbafd
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-11-28 13:15:17 +0000

    iplug-dsc-scripted: - small fix for gs soil which is commented here, to solve problem with missing nativekey in gazetteer locations (queried by extended search as "areaid")

--- src/main/resources/mapping/igc-3.2.0_to_lucene.js
+++ src/main/resources/mapping/igc-3.2.0_to_lucene.js
@@ -645,7 +645,7 @@ function addSpatialRefValue(row) {
     // map "name_value" also as location !
     IDX.add("spatial_ref_value.name_value", row.get("name_value"));
     IDX.add("location", row.get("name_value"));
-    // map "nativekey" also as areaid !
+    // map "nativekey" also as areaid ! queried via extended search gazetteer !
     IDX.add("spatial_ref_value.nativekey", row.get("nativekey"));
     IDX.add("areaid", row.get("nativekey"));
 
@@ -678,6 +678,10 @@ function addSpatialRefValue(row) {
 }
 function addSpatialRefSns(row) {
     IDX.add("spatial_ref_sns.sns_id", row.get("sns_id"));
+    // GS Soil FIX !!! map id of gazetteer Location also as areaid, cause NO "nativekey" set in Location at the moment !
+    // Extended search portal uses this id as areaid if no nativekey set.
+    // Should be solved in the future when gazetteer also delivers nativekey which then is set as areaid (see above).
+//    IDX.add("areaid", row.get("sns_id"));
     IDX.add("spatial_ref_sns.expired_at", row.get("expired_at"));
 }
 function addSpatialSystem(row) {
