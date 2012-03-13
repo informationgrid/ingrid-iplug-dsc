aa2c97df2b676c52ca4e18ab69f478a0de70653f
ma <ma@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-03-13 09:03:02 +0000

    iplug-dsc-scripted: - fixed double escaping of xml files.

--- src/main/java/de/ingrid/iplug/dsc/utils/DOMUtils.java
+++ src/main/java/de/ingrid/iplug/dsc/utils/DOMUtils.java
@@ -153,7 +153,7 @@ public class DOMUtils {
 	        if (e.hasChildNodes()) {
 	            for (int i=0; i<e.getChildNodes().getLength(); i++) {
 	                if (e.getChildNodes().item(i) instanceof Text) {
-	                    ((Text)e.getChildNodes().item(i)).setNodeValue(escapeXmlText(text));
+	                    ((Text)e.getChildNodes().item(i)).setNodeValue(unNullify(text));
 	                    return this;
 	                }
 	            }
@@ -181,7 +181,7 @@ public class DOMUtils {
 	}
 
     public IdfElement addAttribute(Element element, String attrName, String attrValue) {
-        element.setAttribute(attrName, escapeXmlText(attrValue));
+        element.setAttribute(attrName, unNullify(attrValue));
         return new IdfElement(element);
     }
 
@@ -299,15 +299,15 @@ public class DOMUtils {
 	}
 	
 	private Text domNewTextNode(String data) {
-		return myDoc.createTextNode(escapeXmlText(data));
+		return myDoc.createTextNode(unNullify(data));
 	}
 
-	/** Escapes given text. NOTICE: returns empty string if passed text is null. */
-	private String escapeXmlText(String text) {
+	/** NOTICE: returns empty string if passed text is null. Used to be named escapeXmlText with StringEscapeUtils.escapeXml(text) as return value*/
+	private String unNullify(String text) {
 		if (text == null) {
 			text = "";
 		}
-		return StringEscapeUtils.escapeXml(text);
+		return text;
 	}
 
 }
