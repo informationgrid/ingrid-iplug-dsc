0ad33a9f68ea37df472efd0efc734d39e55aced1
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-05-04 15:51:46 +0000

    - fixed SQL query to get all necessary fields

--- src/main/resources/mapping/igc-3.2.0_to_idf.js
+++ src/main/resources/mapping/igc-3.2.0_to_idf.js
@@ -688,7 +688,7 @@ for (i=0; i<objRows.size(); i++) {
     
     if (objClass.equals("3")) {
         // ---------- <gmd:identificationInfo/srv:coupledResource/srv:SV_CoupledResource/srv:identifier/gco:CharacterString> ----------
-        var rows = SQL.all("SELECT object_reference.obj_to_uuid, t01_object.org_obj_id, t01_object.id FROM object_reference, t01_object WHERE object_reference.obj_to_uuid=t01_object.obj_uuid AND obj_from_id=? AND special_ref=? AND t01_object.work_state=?", [objId, '3210', "V"]);
+        var rows = SQL.all("SELECT t01_object.* FROM object_reference, t01_object WHERE object_reference.obj_to_uuid=t01_object.obj_uuid AND obj_from_id=? AND special_ref=? AND t01_object.work_state=?", [objId, '3210', "V"]);
         var resourceIdentifiers = [];
         for (i=0; i<rows.size(); i++) {
             var refObjId = rows.get(i).get("id");
