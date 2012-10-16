9402f37ab2e6beff77ab082ab4c1fe3ca3fdc259
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-10-16 09:29:34 +0000

    Add java parameter "-Dfile.encoding=UTF8" to start.sh.

--- src/main/release/scripts/start.sh
+++ src/main/release/scripts/start.sh
@@ -171,7 +171,7 @@ startIplug()
 
   # run it
   export CLASSPATH="$CLASSPATH"
-  INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME"
+  INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME -Dfile.encoding=UTF8"
   CLASS=de.ingrid.admin.JettyStarter
 	
   exec nohup "$JAVA" $JAVA_HEAP_MAX $INGRID_OPTS $CLASS > console.log & 
