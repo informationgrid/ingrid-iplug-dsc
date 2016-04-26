0b38982839e62ed2f8182061756286332acda294
Martin Maidhof <martin@wemove.com>
2016-04-26 19:20:38 +0200

    - Fix wrong configuration so iPlug runs immediately - add example spring xml in override for setup - comment example mapping files

--- src/main/resources/mapping/example_to_lucene.js
+++ src/main/resources/mapping/example_to_lucene.js
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,62 +27,53 @@ if (javaVersion.indexOf( "1.8" ) === 0) {
 importPackage(Packages.org.apache.lucene.document);
 importPackage(Packages.de.ingrid.iplug.dsc.om);
 importPackage(Packages.de.ingrid.geo.utils.transformation);
+importPackage(Packages.de.ingrid.iplug.wfs.dsc.tools);
 
 if (log.isDebugEnabled()) {
-	log.debug("Mapping source record to lucene document: " + sourceRecord.toString());
+  log.debug("Mapping source record to lucene document: " + sourceRecord.toString());
 }
 
 if (!(sourceRecord instanceof DatabaseSourceRecord)) {
-    throw new IllegalArgumentException("Record is no DatabaseRecord!");
+  throw new IllegalArgumentException("Record is no DatabaseRecord!");
 }
 
-// ---------- bundesland ----------
+// ---------- MAP RECORD INTO INDEX ----------
+
+// extract id of the record and read record(s) from database
 var objId = sourceRecord.get("id");
-var objRows = SQL.all("SELECT * FROM bundesland WHERE id=?", [objId]);
+var objRows = SQL.all("SELECT * FROM [YOUR TABLE NAME] WHERE id=?", [objId]);
 for (i=0; i<objRows.size(); i++) {
     var row = objRows.get(i);
-    var title = "Stammdaten BUNDESLAND: ";
+    var title = "";
     var summary = "";
-	var geobasUrl = "http://10.140.105.57:8080/geobas_q1/main?cmd=view_details&id=";
 
-    IDX.add("bundesland.id", row.get("id"));
-    IDX.add("bundesland.kurzbezeichnung", row.get("kurzbezeichnung"));
-    IDX.add("bundesland.name", row.get("name"));
-    IDX.add("bundesland.organisation", row.get("organisation"));
+    // Map id and data from record(s) into index
+    IDX.add("id", row.get("id"));
+    IDX.add("kurzbeschreibung", row.get("kurzbeschreibung"));
+    IDX.add("daten", row.get("daten"));
+    IDX.add("organisation", row.get("behoerde"));
+    // ...
 
-	title = title + row.get("name") + ", " + row.get("kurzbezeichnung");
-	summary = summary + row.get("name") + ", " + row.get("kurzbezeichnung");
-	geobasUrl = geobasUrl + row.get("id") + "&table=bundesland";
-
-    // ---------- organisation ----------
-    var rows = SQL.all("SELECT * FROM organisation WHERE id=?", [row.get("organisation")]);
-    for (j=0; j<rows.size(); j++) {
-    	var row = rows.get(j);
-        IDX.add("organisation.id", row.get("id"));
-        IDX.add("organisation.name", row.get("name"));
-    	summary = summary + ", " + row.get("name");
-   }
-
-	IDX.add("title", title);
+    // these fields are shown in result list !
+    title = title + row.get("daten");
+    summary = summary + row.get("kurzbeschreibung");
+    IDX.add("title", title);
     IDX.add("summary", summary);
-    
+
+    // add various HTML into special index field which is rendered in result list !
+    var addHtml = "";
+    var sourceTypes = ["WMS", "Dateidownload", "FTP", "AtomFeeed", "Portal", "SOS", "WFS", "WMTS", "JSON", "WSDL"];
+    for (var i = 0; i < sourceTypes.length; i++) {
+        var key = sourceTypes[i].toLowerCase();
+        if( hasValue(row.get(key))){
+            addHtml += "<div><a href="+row.get(key)+">"+sourceTypes[i]+"</></div>";
+        }
+    }
+    IDX.add("additional_html_1", addHtml);
+
     // deliver url or NOT !?
     // changes display in result list !
     // with URL the url is displayed below summary and title links to URL
     // without url title links to detail view !
-    //IDX.add("url", geobasUrl);
-}
-
-function hasValue(val) {
-    if (typeof val == "undefined") {
-        return false; 
-    } else if (val == null) {
-        return false; 
-    } else if (typeof val == "string" && val == "") {
-        return false;
-    } else if (typeof val == "object" && val.toString().equals("")) {
-        return false;
-    } else {
-      return true;
-    }
+    //IDX.add("url", "[YOUR URL]");
 }
