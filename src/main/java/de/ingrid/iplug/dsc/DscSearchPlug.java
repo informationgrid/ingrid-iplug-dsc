7bee1a6fb756305b056972f97a73cd39cb068afa
Andre Wallat <andre.wallat@wemove.com>
2018-10-22 16:00:35 +0200

    Implement remote indexing from iBus

--- src/main/java/de/ingrid/iplug/dsc/DscSearchPlug.java
+++ src/main/java/de/ingrid/iplug/dsc/DscSearchPlug.java
@@ -22,33 +22,24 @@
  */
 package de.ingrid.iplug.dsc;
 
-import java.io.IOException;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.tngtech.configbuilder.ConfigBuilder;
-
 import de.ingrid.admin.JettyStarter;
+import de.ingrid.admin.elasticsearch.IndexScheduler;
 import de.ingrid.elasticsearch.search.IndexImpl;
 import de.ingrid.iplug.HeartBeatPlug;
 import de.ingrid.iplug.IPlugdescriptionFieldFilter;
 import de.ingrid.iplug.PlugDescriptionFieldFilters;
 import de.ingrid.iplug.dsc.record.DscRecordCreator;
-import de.ingrid.utils.ElasticDocument;
-import de.ingrid.utils.IRecordLoader;
-import de.ingrid.utils.IngridCall;
-import de.ingrid.utils.IngridDocument;
-import de.ingrid.utils.IngridHit;
-import de.ingrid.utils.IngridHitDetail;
-import de.ingrid.utils.IngridHits;
+import de.ingrid.utils.*;
 import de.ingrid.utils.dsc.Record;
 import de.ingrid.utils.metadata.IMetadataInjector;
 import de.ingrid.utils.processor.IPostProcessor;
 import de.ingrid.utils.processor.IPreProcessor;
 import de.ingrid.utils.query.IngridQuery;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * This iPlug connects to the iBus delivers search results based on a index.
@@ -66,19 +57,21 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
 
     public static Configuration conf;
 
-    private DscRecordCreator dscRecordProducer = null;
+    private DscRecordCreator dscRecordProducer;
     
-    private final IndexImpl _indexSearcher;    
-    
+    private final IndexImpl _indexSearcher;
+    private final IndexScheduler indexScheduler;
+
     @Autowired
     public DscSearchPlug(final IndexImpl indexSearcher,
-            IPlugdescriptionFieldFilter[] fieldFilters,
-            IMetadataInjector[] injector, IPreProcessor[] preProcessors,
-            IPostProcessor[] postProcessors, DscRecordCreator producer) throws IOException {
+                         IPlugdescriptionFieldFilter[] fieldFilters,
+                         IMetadataInjector[] injector, IPreProcessor[] preProcessors,
+                         IPostProcessor[] postProcessors, DscRecordCreator producer, IndexScheduler indexScheduler) {
         super(60000, new PlugDescriptionFieldFilters(fieldFilters), injector,
                 preProcessors, postProcessors);
         _indexSearcher = indexSearcher;
         dscRecordProducer = producer;
+        this.indexScheduler = indexScheduler;
     }
 
 
@@ -110,7 +103,7 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
      * @see de.ingrid.iplug.HeartBeatPlug#close()
      */
     @Override
-    public void close() throws Exception {
+    public void close() {
         _indexSearcher.close();
     }
 
@@ -119,19 +112,16 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
      */
     @Override
     public IngridHitDetail getDetail(IngridHit hit, IngridQuery query,
-            String[] fields) throws Exception {
-        final IngridHitDetail detail = _indexSearcher.getDetail(hit, query,
-                fields);
-        return detail;
+            String[] fields) {
+        return _indexSearcher.getDetail(hit, query, fields);
     }
 
     /* (non-Javadoc)
      * @see de.ingrid.iplug.HeartBeatPlug#close()
      */
     @Override
-    public IngridHitDetail[] getDetails(IngridHit[] hits, IngridQuery query, String[] fields) throws Exception {
-        final IngridHitDetail[] details = _indexSearcher.getDetails(hits, query, fields);
-        return details;
+    public IngridHitDetail[] getDetails(IngridHit[] hits, IngridQuery query, String[] fields) {
+        return _indexSearcher.getDetails(hits, query, fields);
     }
     
     public DscRecordCreator getDscRecordProducer() {
@@ -143,14 +133,22 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
     }
     
     public static void main(String[] args) throws Exception {
-        conf = new ConfigBuilder<Configuration>(Configuration.class).withCommandLineArgs(args).build();
+        conf = new ConfigBuilder<>(Configuration.class).withCommandLineArgs(args).build();
         new JettyStarter( conf );
     }
 
-
     @Override
-    public IngridDocument call(IngridCall targetInfo) throws Exception {
-        throw new RuntimeException( "call-function not implemented in DSC-iPlug" );
+    public IngridDocument call(IngridCall info) {
+        IngridDocument doc = null;
+
+        if ("index".equals(info.getMethod())) {
+            indexScheduler.triggerManually();
+            doc = new IngridDocument();
+            doc.put("success", true);
+        }
+        log.warn("The following method is not supported: " + info.getMethod());
+
+        return doc;
     }
     
 }
