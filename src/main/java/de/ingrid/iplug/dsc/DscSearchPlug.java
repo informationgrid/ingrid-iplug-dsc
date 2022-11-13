83253c245344a52bec23d684cb5d21dcff527fc7
André Wallat <andre.wallat@wemove.com>
2022-11-13 18:42:50 +0100

    Adapt to new base-webapp

--- src/main/java/de/ingrid/iplug/dsc/DscSearchPlug.java
+++ src/main/java/de/ingrid/iplug/dsc/DscSearchPlug.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,7 @@
  */
 package de.ingrid.iplug.dsc;
 
-import de.ingrid.admin.JettyStarter;
+import de.ingrid.admin.Config;
 import de.ingrid.admin.elasticsearch.IndexScheduler;
 import de.ingrid.elasticsearch.ElasticConfig;
 import de.ingrid.elasticsearch.IBusIndexManager;
@@ -43,20 +43,35 @@ import de.ingrid.utils.query.IngridQuery;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
+
+import java.io.IOException;
 
 /**
  * This iPlug connects to the iBus delivers search results based on a index.
  *
  * @author joachim@wemove.com
  */
-@Service
+@ImportResource({"/springapp-servlet.xml", "/override/*.xml"})
+@SpringBootApplication(scanBasePackages = "de.ingrid")
+@ComponentScan(
+        basePackages = "de.ingrid",
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "de.ingrid.admin.object.DefaultDataType"),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "de.ingrid.admin.object.BasePlug"),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "de.ingrid.admin.BaseWebappApplication"),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "de.ingrid.admin.controller.RedirectController"),
+        })
 public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
 
     /**
      * The logging object
      */
-    private static Log log = LogFactory.getLog(DscSearchPlug.class);
+    private static final Log log = LogFactory.getLog(DscSearchPlug.class);
 
     @Autowired
     private ElasticConfig elasticConfig;
@@ -72,8 +87,12 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
     private final IndexImpl _indexSearcher;
     private final IndexScheduler indexScheduler;
 
+    private final Config baseConfig;
+
     @Autowired
     public DscSearchPlug(final IndexImpl indexSearcher,
+                         Config baseConfig,
+                         Configuration externalConfig,
                          IPlugdescriptionFieldFilter[] fieldFilters,
                          IMetadataInjector[] injector, IPreProcessor[] preProcessors,
                          IPostProcessor[] postProcessors, DscRecordCreator producer, IndexScheduler indexScheduler) {
@@ -82,6 +101,18 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
         _indexSearcher = indexSearcher;
         dscRecordProducer = producer;
         this.indexScheduler = indexScheduler;
+
+        this.baseConfig = baseConfig;
+        try {
+            baseConfig.initialize();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (externalConfig != null) {
+            externalConfig.initialize();
+        } else {
+            log.info("No external configuration found.");
+        }
     }
 
 
@@ -104,7 +135,7 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
         if (elasticConfig.esCommunicationThroughIBus) {
 
             ClauseQuery cq = new ClauseQuery(true, false);
-            cq.addField(new FieldQuery(true, false, "iPlugId", JettyStarter.baseConfig.communicationProxyUrl));
+            cq.addField(new FieldQuery(true, false, "iPlugId", baseConfig.communicationProxyUrl));
             query.addClause(cq);
             return this.iBusIndexManager.search(query, start, length);
         }
@@ -175,7 +206,7 @@ public class DscSearchPlug extends HeartBeatPlug implements IRecordLoader {
     }
 
     public static void main(String[] args) throws Exception {
-        new JettyStarter(Configuration.class);
+        SpringApplication.run(DscSearchPlug.class, args);
     }
 
     @Override
